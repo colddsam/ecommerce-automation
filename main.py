5f5174c32062b95bbd93ca2075d9277011cbad1b
colddsam <dassamratkumar772@gmail.com>
2024-10-07 21:10:10 +0530

    making the thing ready for github action

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ endpoints = {
 }
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage")  
+chrome_options.add_argument("--window-size=1920,1080")  
+chrome_options.add_argument("--disable-gpu") 
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
+
 driver = webdriver.Chrome(options=chrome_options)
 
 
